6ee65a5d91ec61ac10afadd7113b792a53e71e7f
Marc Llopart <marc@medullar.com>
2025-04-15 07:50:39 +0200

    add camera limits

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -4,6 +4,8 @@ class_name Player extends CharacterBody2D
 var cardinal_direction: Vector2 = Vector2.DOWN
 var direction: Vector2 = Vector2.ZERO
 
+const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var state_machine: PlayerStateMachine = $StateMachine
@@ -13,37 +15,40 @@ signal DirectionChanged(new_direction: Vector2)
 func _ready() -> void:
 	state_machine.Initialize(self)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
 func SetDirection() -> bool:
-	var new_direction: Vector2 = cardinal_direction
+	#var new_direction: Vector2 = cardinal_direction
 
 	if direction == Vector2.ZERO:
 		return false
+		
+	var direction_id:int = int(round((direction + cardinal_direction * 0.1).angle() / TAU * DIR_4.size()))
+	var new_direction = DIR_4[direction_id]
 
-	if direction.y == 0:
-		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
-	elif direction.x > 0 and direction.y < 0: # Handles up-right direction
-		if cardinal_direction == Vector2.DOWN || cardinal_direction == Vector2.LEFT:
-			new_direction = Vector2.RIGHT
-	elif direction.x > 0 and direction.y > 0: # Handles down-right direction
-		if cardinal_direction == Vector2.UP || cardinal_direction == Vector2.LEFT:
-			new_direction = Vector2.RIGHT
-	elif direction.x < 0 and direction.y < 0: # Handles up-left direction
-		if cardinal_direction == Vector2.DOWN || cardinal_direction == Vector2.RIGHT:
-			new_direction = Vector2.LEFT
-	elif direction.x < 0 and direction.y > 0: # Handles down-left direction
-		if cardinal_direction == Vector2.UP || cardinal_direction == Vector2.RIGHT:
-			new_direction = Vector2.LEFT
+	#if direction.y == 0:
+		#new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	#elif direction.x == 0:
+		#new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	#elif direction.x > 0 and direction.y < 0: # Handles up-right direction
+		#if cardinal_direction == Vector2.DOWN || cardinal_direction == Vector2.LEFT:
+			#new_direction = Vector2.RIGHT
+	#elif direction.x > 0 and direction.y > 0: # Handles down-right direction
+		#if cardinal_direction == Vector2.UP || cardinal_direction == Vector2.LEFT:
+			#new_direction = Vector2.RIGHT
+	#elif direction.x < 0 and direction.y < 0: # Handles up-left direction
+		#if cardinal_direction == Vector2.DOWN || cardinal_direction == Vector2.RIGHT:
+			#new_direction = Vector2.LEFT
+	#elif direction.x < 0 and direction.y > 0: # Handles down-left direction
+		#if cardinal_direction == Vector2.UP || cardinal_direction == Vector2.RIGHT:
+			#new_direction = Vector2.LEFT
 
 	if new_direction == cardinal_direction:
 		return false
---
+++ TileMaps/level_tile_map.gd
@@ -0,0 +1,13 @@
+class_name LevelTileMap extends TileMap
+
+
+func _ready() -> void:
+	LevelManager.ChangeTilemapBounds(GetTileMapBounds())
+	
+func GetTileMapBounds() -> Array[Vector2]:
+	var bounds:Array[Vector2] = []
+	
+	bounds.append(Vector2(get_used_rect().position * rendering_quadrant_size))
+	bounds.append(Vector2(get_used_rect().end * rendering_quadrant_size))
+	
+	return bounds
